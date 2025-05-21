c3076f031a070c6611f279c8313dcb82f4df4396
dovahk11m <choongechobiz@gamil.com>
2025-05-21 18:05:06 +0900

    Students/Exception/조충희/5.21

--- src/service/StudentsService.java
+++ src/service/StudentsService.java
@@ -9,12 +9,18 @@ public class StudentsService {
     private final StudentsDAO studentsDAO = new StudentsDAO();
 
     //1.학생 전체 조회
-    public List<Students> getAllStudent() throws SQLException {
-        return studentsDAO.getAllStudents();
+    public List<Students> getAllStudent() {
+        try {
+            return studentsDAO.getAllStudents();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }//
 
     //2.학생 이름 조회
-    public List<Students> searchByName(String studentName) throws SQLException {
+    public List<Students> searchByName(String studentName) {
+
+        try {
 
         //입력값 유효성 검사
         if (studentName == null || studentName.trim().isEmpty()) {
@@ -22,26 +28,44 @@ public class StudentsService {
         }
         //유효성 검사 완료, studentDAO에 협업 요청
         return studentsDAO.searchByName(studentName);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }//
 
     //3.학생 로그인 조회
-    public Students authenticateStudent(String studentId) throws SQLException {
+    public Students authenticateStudent(String studentId) {
+
+        try {
+
         //입력값 유효성 검사
         if (studentId == null || studentId.trim().isEmpty()) {
             throw new SQLException("유효한 id 값을 다시 입력해주세요");
         }
         //유효성 검사 완료, studentDAO에 협업 요청
         return studentsDAO.authenticateStudent(studentId);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }//
 
     //4.학생 수강 조회
-    public Students studentCourse(String studentId) throws SQLException {
+    public Students studentCourse(String studentId) {
+
+        try {
+
         //입력값 유효성 검사
         if (studentId == null || studentId.trim().isEmpty()) {
             throw new SQLException("유효한 id 값을 다시 입력해주세요");
         }
         //유효성 검사 완료, studentDAO에 협업 요청
         return studentsDAO.studentCourse(studentId);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }//
 
     //테스트코드
@@ -60,13 +84,8 @@ public class StudentsService {
 //        }
 
         //이름조회
-//        try {
-//            System.out.println(service.searchByName("김"));
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
 
-
+            System.out.println(service.searchByName(" "));
 
 
         }//main
