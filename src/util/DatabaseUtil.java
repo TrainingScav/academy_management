f031b5ebe24a88ccadffa9268069668c84b35f1d
HHG_DEV <dnslwk123@naver.com>
2025-05-21 09:45:41 +0900

    databaseUtil 수정

--- src/util/DatabaseUtil.java
+++ src/util/DatabaseUtil.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
  *  데이터 베이스 연결을 관리하는 유틸리티 클래스
  */
 public class DatabaseUtil {
-    private static  final String DB_URL = "jdbc:mysql://192.168.0.184:3306/mydatabase?serverTimezone=Asia/Seoul";
+    private static  final String DB_URL = "jdbc:mysql://192.168.0.184:3306/academy_management?serverTimezone=Asia/Seoul";
     private static final String DB_USER = "academy_admin";
     private static final String DB_PASSWORD = "ghkdlxld";
 
